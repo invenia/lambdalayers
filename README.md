314d8036613947eb027c6720b8e909294a475449
Eric Davies <eric.davies@invenia.ca>
2019-02-27 22:49:58 +0000

    Add tests

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lambda Layers
+
+[![Build Status](https://gitlab.invenia.ca/infrastructure/lambdalayers/badges/master/build.svg)](https://gitlab.invenia.ca/infrastructure/lambdalayers/commits/master)
+[![Coverage Status](https://gitlab.invenia.ca/infrastructure/lambdalayers/badges/master/coverage.svg)](https://infrastructure.pages.invenia.ca/lambdalayers/coverage/)
+[![Python Version](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+
+Some useful AWS Lambda layers for Invenia (and code to deploy them)
--- setup.py
+++ setup.py
@@ -2,22 +2,23 @@ from setuptools import find_packages, setup
 
 from lambdalayers.version import __version__
 
-TEST_DEPS = ["coverage", "pytest", "pytest-cov"]
+
+TEST_DEPS = ["coverage", "pytest", "pytest-cov", "requests"]
 CHECK_DEPS = ["isort", "flake8", "flake8-quotes", "pep8-naming", "black", "mypy"]
-REQUIREMENTS = ["plz", "boto3"]
+REQUIREMENTS = [
+    "plz @ git+https://gitlab.invenia.ca/infrastructure/plz@master#egg=plz",
+    "boto3",
+]
 
 EXTRAS = {"test": TEST_DEPS, "check": CHECK_DEPS, "dev": TEST_DEPS + CHECK_DEPS}
 
 setup(
-    name="Lambda Layers",
+    name="lambdalayers",
     version=__version__,
     description="Some useful AWS Lambda layers for Invenia (and code to deploy them)",
     author="Invenia Technical Computing",
     url="https://gitlab.invenia.ca/infrastructure/lambdalayers",
     packages=find_packages(exclude=["tests"]),
-    dependency_links=[
-        "git+https://gitlab.invenia.ca/infrastructure/plz@master#egg=plz"
-    ],
     install_requires=REQUIREMENTS,
     tests_require=TEST_DEPS,
     extras_require=EXTRAS,
